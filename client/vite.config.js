ab2dd85ab3a9c2e639e2c3e20a7724c5e50748e1
AshishRam7 <jaashishram7@gmail.com>
2025-03-28 22:37:40 +0530

    change made

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,8 @@ import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json' assert { type: 'json' };
 
 export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest }),
-  ],
-})
+  plugins: [react(), crx({ manifest })],
+  server: {
+    port: 3000, // set the port to 3000
+  },
+});
